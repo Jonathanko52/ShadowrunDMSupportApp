37b04e8d460c54b74d2b5ddd12679a568a478aa8
Jonathan Ko <jonathanko@Jonathans-MacBook-Pro.local>
2018-10-30 12:28:55 -0700

    fetch fix?

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 const htmlWebPackPlugin = new HtmlWebPackPlugin({
-  template: "./views/index.html",
+  template: "./views/login.html",
   filename: "./login.html"
 });
 const miniCssExtractPlugin = new MiniCssExtractPlugin({
